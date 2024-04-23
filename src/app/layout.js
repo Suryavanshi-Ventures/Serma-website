dd188af6e5517ffdef2cd7150d04d7271abc904f
pdpatel1288 <141713687+pdpatel1288@users.noreply.github.com>
2024-04-23 11:30:21 +0530

    added helvetica fonts

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body >
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
           <Header />
           {children}
