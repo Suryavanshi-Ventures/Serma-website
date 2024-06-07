2cd89fdbe5fef2a7165e7984ab3bceef413cc837
email <sahushankar8202@gmail.com>
2024-06-07 15:07:46 +0530

    fsf

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -26,7 +26,7 @@ const Header = () => {
   };
 
   return (
-    <header className=" relative pt-[40px] pb-[14px] px-[20px] lg:px-[85px]">
+    <header className=" relative pt-[40px] pb-[40px] px-[20px] lg:px-[85px]">
       <div className="flex justify-between  items-center w-full gap-0  xxl:gap-10  ">
         <div className="">
           <Link href="/">
